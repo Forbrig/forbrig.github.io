cc8323ecb63b263dfbae59ef7acfafa672f8f4ad
Forbrig <vitorforbrig@gmail.com>
2019-05-14 10:11:10 -0300

    iframe test

--- js/script.js
+++ js/script.js
@@ -7,5 +7,11 @@ $('#aboutBtn').click(function() {
 $('#portfolioBtn').click(function() {
     $('#about').hide();
     $('#portfolio').show();
-    $('#portfolio').load("/GAMES-2019-T1/theGame/export/html5/bin/");
+    // $('#openfl-content > canvas').attr('height', '100%');
+    // var canvas = $('#openfl-content > canvas');
+    var canvas = $('#portfolio').contents().find('#openfl-content > canvas');
+    // canvas.removeProperty('width');
+    canvas.width = $("#portfolio").width();
+    canvas.css('width', '100%');
+    // $('#portfolio').load("https://forbrig.github.io/GAMES-2019-T1/theGame/export/html5/bin/");
 });
