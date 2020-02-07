95c23eba9252265a19f5a3b8918c0f444a377ace
vitor <vitor@amplisoftware.com>
2020-02-07 08:02:58 -0300

    React Portfolio: Commit 1

---
+++ portfolio/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './global.css';
+
+import NavBar from './components/NavBar'; 
+import Home from './components/Home'; 
+
+function App() {
+  return (
+    <div className="App">
+          <NavBar></NavBar>
+    </div>
+  );
+}
+
+export default App;
