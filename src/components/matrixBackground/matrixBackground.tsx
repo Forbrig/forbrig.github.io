705588d1c3556c23c0f969fde93d9bfd22eb3639
Vitor Forbrig <vitorforbrig@gmail.com>
2025-07-03 22:36:37 -0300

    Refactor MatrixBackground component for improved responsiveness and code clarity

--- src/components/matrixBackground/matrixBackground.tsx
+++ src/components/matrixBackground/matrixBackground.tsx
@@ -2,16 +2,26 @@
 
 import { useContext, useEffect, useRef, useState } from "react";
 
-import styles from "./matrixBackground.module.scss";
 import { themeContext } from "@/context/ThemeProvider";
 
+import styles from "./matrixBackground.module.scss";
+
 export const MatrixBackground = () => {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
   const { theme } = useContext(themeContext);
 
   const cavasRef = useRef<HTMLCanvasElement | null>(null);
 
-  const [currentWidth, setCurrentWidth] = useState(window.innerWidth);
-  const [currentHeight, setCurrentHeight] = useState(window.innerHeight);
+  const [currentWidth, setCurrentWidth] = useState(0);
+  const [currentHeight, setCurrentHeight] = useState(0);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setCurrentWidth(window.innerWidth);
+      setCurrentHeight(window.innerHeight);
+    }
+  }, []);
 
   useEffect(() => {
     const canvas = cavasRef.current;
@@ -20,20 +30,19 @@ export const MatrixBackground = () => {
     const ctx = canvas.getContext("2d");
     if (!ctx) return;
 
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    canvas.width = currentWidth;
+    canvas.height = currentHeight;
 
     const fontSize = 16;
     const columns = canvas.width / fontSize;
 
     const drops: number[] = Array.from({ length: columns }, () => 1);
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
     const draw = () => {
       ctx.fillStyle = "rgba(0, 0, 0, 0.08)"; // Semi-transparent black for fading effect
       ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-      ctx.fillStyle = "#0F0"; // Green color for the characters
+      ctx.fillStyle = "#00ff00"; // Green color for the characters
       ctx.font = `${fontSize}px monospace`;
 
       for (let i = 0; i < drops.length; i++) {
@@ -51,14 +60,16 @@ export const MatrixBackground = () => {
     const interval = setInterval(draw, 30);
 
     const handleResize = () => {
+      if (typeof window === "undefined") return;
+
       setCurrentWidth(window.innerWidth);
       setCurrentHeight(window.innerHeight);
     };
 
-    window.addEventListener("resize", handleResize);
+    window?.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener("resize", handleResize);
+      window?.removeEventListener("resize", handleResize);
       clearInterval(interval);
     };
   }, [theme, currentWidth, currentHeight]);
