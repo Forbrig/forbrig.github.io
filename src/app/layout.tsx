e8cfa834dac539fc88722b6d0a1325ff0f9490aa
Vitor Forbrig <vitorforbrig@gmail.com>
2025-07-09 16:52:49 -0300

    Update metadata in layout: enhance title and description for improved SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,11 +9,30 @@ import { MatrixBackground } from "@/components/matrixBackground";
 import "./globals.scss";
 import styles from "./layout.module.scss";
 
+const description =
+  "Vitor Forbrig is a Senior Frontend Engineer specializing in React, TypeScript, and modern web technologies. Explore his portfolio and projects.";
+
 export const metadata: Metadata = {
-  title: "Vitor Forbrig",
-  description: "Vitor Forbrig - Senior Frontend Engineer",
+  title: "Vitor Forbrig – Senior Frontend Engineer & JavaScript Specialist",
+  description: description,
   icons: {
     icon: "/cartoon-profile.png",
+  },
+  openGraph: {
+    title: "Vitor Forbrig – Senior Frontend Engineer & JavaScript Specialist",
+    description: description,
+    url: "https://forbrig.github.io/",
+    siteName: "Vitor Forbrig - Portfolio",
+    images: [
+      {
+        url: "/cartoon-profile.png",
+        width: 1200,
+        height: 630,
+        alt: "Vitor Forbrig Cartoon Profile",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
   },
   other: {
     "google-site-verification": "PBsFeMdlBNvFg7Ydt98xQBcbCBF6ms5zWQcfXtTI2sk",
