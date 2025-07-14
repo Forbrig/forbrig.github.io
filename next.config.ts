c625b50188fcac8d94394c11b4e2c5ee8877ec64
Vitor Forbrig <vitorforbrig@gmail.com>
2025-07-14 12:45:40 -0300

    Removes turbopack configuration

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   webpack: (config) => {
-    // Add rule for SVG files
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
     return config;
-  },
-  turbopack: {
-    rules: {
-      "*.svg": {
-        loaders: [
-          {
-            loader: "@svgr/webpack",
-            options: {
-              icon: true,
-            },
-          },
-        ],
-        as: "*.js",
-      },
-    },
   },
 };
 
